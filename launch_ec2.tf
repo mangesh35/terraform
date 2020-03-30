2ea05912fd55ae52c77b6838cac353b0a4332e5d
mangesh35 <59336648+mangesh35@users.noreply.github.com>
2020-03-31 02:36:24 +0530

    Create launch_ec2.tf

---
+++ launch_ec2.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+access_key = "AKIAASDFASDFASDFASDFASDF7DC53R"
+secret_key = "6GiPRRX5bBqoqSmdh4WOdf+zanqYaFNE2asdfasdfasdffdfd4Z0ziWf"
+region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+count = 2
+ami = "ami-0fc61db8544a617ed"
+instance_type = "t2.micro"
+key_name = "mangesh-test"
+
+
+ tags = {
+	Name = "mangesh-${count.index + 1} "
+	}
+
+}
